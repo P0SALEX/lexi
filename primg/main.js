2fa03c034a5ea0a8aad123fddd9db1fd47c78149
P0SALEX <160787720+P0SALEX@users.noreply.github.com>
2024-06-29 17:26:49 -0600

    dont
    imgdtn

---
+++ primg/main.js
@@ -0,0 +1,11 @@
+const card = document.querySelector('.card-container');
+const audio = document.querySelector('.audio');
+
+card.addEventListener('mouseenter', () => {
+    audio.play();
+});
+
+card.addEventListener('mouseleave', () => {
+    audio.pause();
+    audio.currentTime = 0;
+});
